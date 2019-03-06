507c91b68b97bddd4f9d9131ec646559bba26d0e
Vukashin <vukashin@hotmail.com>
2019-03-06 04:19:38 +0100

    entered homework

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkSubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // your code here
+            Substrings(5);
+            
+        }
+
+       static void Substrings(int n)
+        {
+            var source = "Hello from SEDC CodeCademy v7.0";
+            // your code here (using Console.WriteLine)
+            var newString = source.Substring(0, n);
+            var newString2 = source.Substring(n);
+            Console.WriteLine(newString);
+            Console.WriteLine(newString2);
+           
+        }
+       
+    }
+}
